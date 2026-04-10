252928bf9dd9e8dc9f8888088c9756347099beb6
Aadi7171 <Aadi7171@users.noreply.github.com>
2026-04-10 23:06:17 +0530

    updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Hero from '../components/Hero';
+import ActionCard from '../components/ActionCard';
 import './Pages.css';
 
 const Home = () => {
